b8453625e3286cee4f07317714a8f86857ea7984
m-xuanquy <hi.maixuanquy@gmail.com>
2025-04-26 20:24:21 +0700

    frontend: improved header & background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,23 +3,30 @@ import TranslatorBox from './components/TranslatorBox';
 
 export default function App() {
   return (
-    <>
-      <main className="flex flex-row gap-4 ">
-        <div className="flex flex-col gap-4 w-full">
-          <h1 className="text-2xl font-bold">Text Translator</h1>
+    <div class="bg-gradient-to-r from-emerald-400 via-cyan-400 to-sky-500 h-screen"
+>
+        <header className="text-center mb-4">
+      <h1 className="font-handjet text-8xl uppercase font-bold">Machine Translator</h1>
+      <p className="font-mono">A Statistical Learning project using Transformer architecture</p>
+    </header>
+    
+      <main className="flex flex-row gap-4 justify-center">
+        
+        <div className="gap-4 w-[400px]">
           <TranslatorBox />
         </div>
         <div className="flex flex-col gap-4">
+
         <button
           className="bg-blue-500 text-white justify-center align-middle py-auto my-auto rounded h-[50px] w-[100px]"
           onClick={() => alert('Translate button clicked!')}
         >Translate</button>
         </div>
-        <div className="flex flex-col gap-4 w-full">
-          <h1 className="text-2xl font-bold">Text Translator</h1>
+
+        <div className="gap-4 w-[400px]">
           <TranslatorBox />
         </div>
       </main>
-    </>
+    </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "handjet": ['Handjet', 'sans-serif']
+    }
+    },
   },
   plugins: [],
 }
