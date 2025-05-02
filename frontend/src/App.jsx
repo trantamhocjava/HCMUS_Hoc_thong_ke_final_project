cab6ae0f6fe97148952dcdb8f39157e4f123b4a6
m-xuanquy <hi.maixuanquy@gmail.com>
2025-05-02 13:35:48 +0700

    frontend: disable button whilte running, responsive view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,26 +36,26 @@ export default function App() {
 
   return (
     
-    <div className=" h-screen py-10 bg-gradient-to-r from-[#186a5e] to-[#053339] text-[#e1eeeb]" >
+    <div className=" h-screen py-10 bg-gradient-to-r from-[#186a5e] to-[#053339] text-[#e1eeeb] min-w-[700px]" >
 
-      <div className="bg-gradient-to-r from-[#0b201d] to-[#021619] rounded-3xl shadow-lg flex-col w-fit mx-auto p-12 gap-4">
+      <div className="bg-gradient-to-r from-[#0b201d] to-[#021619] rounded-3xl shadow-lg flex-col w-[70%] mx-auto p-12 gap-4">
       <header className="text-center mb-10">
         <h1 className="font-handjet text-8xl uppercase font-bold">Machine Translator</h1>
         <p className="font-mono">A Statistical Learning project using Transformer architecture</p>
       </header>
 
-      <main className="flex flex-row gap-4 justify-center">
-        <form className="flex gap-4" onSubmit={handleSubmit}>
+      <main className="flex flex-col gap-4 justify-center">
+        <form className="flex justify-center align-middle  flex-col md:flex-row gap-4 w-[100%]" onSubmit={handleSubmit}>
 
         <TranslatorBox text={text} setText={setText} placeholder={"Enter text to translate..."}/>
 
         <button
-          className="bg-[#54a399] hover:bg-[#3c8379] hover:border-[#3c8379] font-handjet text-xl text-white justify-center align-middle py-auto my-auto rounded h-[50px] w-[100px] " type="submit"
+          className="bg-[#54a399] hover:bg-[#3c8379] hover:border-[#3c8379] font-handjet text-xl text-white justify-center align-middle py-auto my-auto rounded h-[50px] w-[100px] mx-auto" type="submit" disabled={loading}
         >{loading ? 'Loading...' : 'Translate'}</button>
         
-        </form>
+        
         <TranslatorBox text={translatedText} disabled={true}  />
-
+        </form>
       </main>
       </div>
     </div>
--- frontend/src/components/TranslatorBox.jsx
+++ frontend/src/components/TranslatorBox.jsx
@@ -1,12 +1,17 @@
 export default function TranslatorBox({ text, setText, placeholder, disabled }) {
   return (
+    <div className="flex flex-col justify-center align-middle w-[80%] md:w-[45%] mx-auto my-auto">
+      <label className="text-center font-mono text-2xl pb-2 text-[#e1eeeb] bg-[#192f2b] rounded-md rounded-b-none shadow-lg border-[#70f5e1] border-2 border-b-0" style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }}>
+        {disabled ? 'Tiếng Việt' : 'English'}
+      </label>  
       <textarea
-        className="w-[400px] h-32 p-2 borderrounded resize-none bg-gradient-to-r from-[#70f5e1] to-[#4df0f8] text-[#021619] font-mono text-lg rounded-md "
+        className="w-[100%] h-32 borderrounded resize-none bg-gradient-to-r from-[#70f5e1] to-[#4df0f8] text-[#021619] font-mono text-lg rounded-md mx-auto rounded-t-none shadow-lg border-[#70f5e1] border-2 border-t-0"
         style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }}
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder={placeholder}
         disabled={disabled}
       />
+    </div>
   );
 }
